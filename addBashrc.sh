2ac36dc69e479dbaa5cbdb61184dd7dd35e48546
Seil Suh <reddiana@gmail.com>
2020-11-21 20:54:40 +0900

    Update addBashrc.sh

--- addBashrc.sh
+++ addBashrc.sh
@@ -5,4 +5,7 @@ set -o vi
 alias d='docker'
 alias k='mickrk8s kubectl'
 alias kubectl='mickrk8s kubectl'
+alias kw='watch "microk8s kubectl get pod -A"'
+alias kww='watch "microk8s kubectl get pod -A | grep -v Running"'
+
 EOF
