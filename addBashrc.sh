7c4475123ffbcfb42b72087913d2d1a06f26ce4a
Dionysus <reddiana@gmail.com>
2020-11-21 22:22:58 +0900

    오타 수정

--- addBashrc.sh
+++ addBashrc.sh
@@ -3,8 +3,8 @@ cat << EOF >> /etc/bash.bashrc
 set -o vi
 
 alias d='docker'
-alias k='mickrk8s kubectl'
-alias kubectl='mickrk8s kubectl'
+alias k='microk8s kubectl'
+alias kubectl='microk8s kubectl'
 alias kw='watch "microk8s kubectl get pod -A"'
 alias kww='watch "microk8s kubectl get pod -A | grep -v Running"'
 
