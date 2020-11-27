62c7a3b24ecbf86f5f7d3f79d2a62d6407ac51bc
서세일 <reddiana@gmail.com>
2020-11-27 04:46:03 +0000

    none

--- kfStart.sh
+++ kfStart.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # kubernetes-version v1.15.2
-minikube start \
+sudo minikube start \
+  --kubernetes-version v1.15.2 \
+  --driver=none
   --extra-config=apiserver.service-account-issuer=api \
   --extra-config=apiserver.service-account-signing-key-file=/var/lib/minikube/certs/apiserver.key \
   --extra-config=apiserver.service-account-api-audiences=api \
-  --kubernetes-version v1.15.2 \
-  --insecure-registry "kubeflow-registry.default.svc.cluster.local:30000" \
-  --insecure-registry "10.0.0.0/24" \
+  #--insecure-registry "kubeflow-registry.default.svc.cluster.local:30000" \
+  #--insecure-registry "10.0.0.0/24" \
   --cpus 6 --memory 14336 --disk-size=200g 
   #--insecure-registry "my.sds.redii.net" \
   #--insecure-registry "registry.kube-system.svc.cluster.local" \
