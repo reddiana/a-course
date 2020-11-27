74f672f447a1269e96746cfe808e2323b9af19da
root <root@red-edu-1.asia-northeast3-a.c.pj-mlops-274505.internal>
2020-11-27 21:24:36 +0000

    뭐 했는지도 모르겠다

--- kfStart.sh
+++ kfStart.sh
@@ -2,14 +2,19 @@
 
 # kubernetes-version v1.15.2
 sudo minikube start \
+  --driver=none \
   --kubernetes-version v1.15.2 \
-  --driver=none
-  --extra-config=apiserver.service-account-issuer=api \
-  --extra-config=apiserver.service-account-signing-key-file=/var/lib/minikube/certs/apiserver.key \
-  --extra-config=apiserver.service-account-api-audiences=api \
+  --insecure-registry "10.0.0.0/24" \
+  --insecure-registry "registry.kube-system.svc.cluster.local" 
+  #--extra-config=apiserver.service-account-issuer=api \
+  #--extra-config=apiserver.service-account-signing-key-file=/var/lib/minikube/certs/apiserver.key \
+  #--extra-config=apiserver.service-account-api-audiences=api
+
+  #--driver=docker \
+  #--cpus 6 --memory 14336 --disk-size=200g \
+
   #--insecure-registry "kubeflow-registry.default.svc.cluster.local:30000" \
   #--insecure-registry "10.0.0.0/24" \
-  --cpus 6 --memory 14336 --disk-size=200g 
   #--insecure-registry "my.sds.redii.net" \
   #--insecure-registry "registry.kube-system.svc.cluster.local" \
 
