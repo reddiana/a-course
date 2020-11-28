17ba0816938e709709259ed4f02840e672e7a952
root <root@red-edu-1.asia-northeast3-a.c.pj-mlops-274505.internal>
2020-11-28 13:53:20 +0000

    되어가는 듯

--- addBashrc.sh
+++ addBashrc.sh
@@ -3,9 +3,13 @@ cat << EOF >> /etc/bash.bashrc
 set -o vi
 
 alias d='docker'
-alias k='microk8s kubectl'
-alias kubectl='microk8s kubectl'
-alias kw='watch "microk8s kubectl get pod -A"'
-alias kww='watch "microk8s kubectl get pod -A | grep -v Running"'
 
+alias k='kubectl'
+alias kw='watch "kubectl get pod -A"'
+alias kww='watch "kubectl get pod -A | grep -v Running"'
+
+# alias k='microk8s kubectl'
+# alias kubectl='microk8s kubectl'
+# alias kw='watch "microk8s kubectl get pod -A"'
+# alias kww='watch "microk8s kubectl get pod -A | grep -v Running"'
 EOF
